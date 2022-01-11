170b99e88ca02c22b1472e30af540c90b5a91a56
Noah2211 <noah.decroix3@gmail.com>
2022-01-11 13:18:47 +0100

    done

--- index.php
+++ index.php
@@ -2,32 +2,42 @@
 
 // Premiere ligne
 
-$fruits = array("Apple", "Banana" "Orange");
-echo ;
-
+$fruits = array("Apple", "Banana", "Orange");
+echo count($fruits)."<br>";
+echo $fruits[1] ;
 //Deuxieme ligne
 
+$age = array("nom" => "Peter", "age" => "35", "nom2" => "Ben","age2" => "37", "nom3" => "Joe","age3" =>  "43");
 
-$age = array("Peter" "35", "Ben" "37", "Joe" "43");
+echo "<br><br>Ben is " .$age["age"]. "years old";
 
-echo "<br><br>Ben is " .  . " years old.<br><br>";
-
-($age  $x  $y) {
-    echo "<br>Key=" .  . ", Value=" . ;
+foreach ($age as $key => $value) {
+    echo "<br><br> $key --> $value" ."<br>";
 }
 
 
 
 //Troisieme ligne
 
-$colors = array("red", "green", "blue", "yellow");
-;
+$colors = ["red", "green", "blue", "yellow"];
+sort($colors);
+foreach ($colors as $values) {
+    echo "<br>".$values ."<br>";
+}
 
+rsort($colors);
+foreach ($colors as $values) {
+    echo "<br>".$values ."<br>";
+}
 
 // Quatrieme ligne
 
-$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-;
+$age = array("Peter" => "35", "Ben" => "37", "Joe" =>"43");
+ksort($age);
+foreach ($age as $key => $values) {
+    echo "$key = $values" ."<br>";
+}
+
 
 
 
